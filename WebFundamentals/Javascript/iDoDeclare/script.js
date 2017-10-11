263465cb6287a8297ba70fdb75518673d3e36808
eboppel <eboppel@gmail.com>
2017-10-11 15:21:34 -0600

    Added JS iDoDeclare Exercise

---
+++ WebFundamentals/Javascript/iDoDeclare/script.js
@@ -0,0 +1,27 @@
+var number1 = 485;
+var number2 = -34;
+var number3 = 3/6;
+var number4 = 0.8746;
+
+var string1 = "We have a winner!";
+var string2 = "Hello World!";
+var string3 = "Jacascript is awesome";
+
+var boolean1 = true;
+var boolean2 = false;
+
+var undef
+
+console.log("The first number is:", number1);
+console.log("The second number is:", number2);
+console.log("The third number is:", number3);
+console.log("and the last number is:", number4);
+
+console.log("The first string:", string1);
+console.log("The second string:", string2);
+console.log("The third string:", string3);
+
+console.log("One of the boolean variables is:", boolean1);
+console.log("and the other boolean variable is:", boolean2);
+
+console.log("And I have no idea what this is:", undef);
